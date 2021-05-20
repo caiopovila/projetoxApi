24b5069e9c3abb4ea9decbfb6f342a5b70e33017
caiopovila <caiopovila@gmail.com>
2021-05-19 22:25:50 -0300

    remove ajustes bodyparser

--- index.ts
+++ index.ts
@@ -1,13 +1,6 @@
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
-import * as helmet from 'helmet';
-
 import { app } from './src/app';
 
 
 const port = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.listen(port, () => console.log('App listado na porta ' + port));
