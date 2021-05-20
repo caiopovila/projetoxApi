e9039755b726c2bcd3c9c2e9b5a76df34bae39d2
caiopovila <caiopovila@gmail.com>
2021-05-19 22:26:55 -0300

    ajustes/autenticação

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
+
+import * as controller from './controller';
 
 
 export const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.json({ 
@@ -9,3 +17,5 @@ app.get('/', (req, res) => {
         version: 1.0
     });
 });
+
+app.post('/auth', controller.user.authUser);
