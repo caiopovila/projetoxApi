d983bb434319f88666120c5f5de4505f8976c3c6
caiopovila <caiopovila@gmail.com>
2021-05-26 15:35:46 -0300

    Add rotas / helmet

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,11 @@ import * as cors from 'cors';
 import * as helmet from 'helmet';
 
 import * as controller from './controller';
-
+import { verify } from './jwt';
 
 export const app = express();
 
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -19,3 +20,21 @@ app.get('/', (req, res) => {
 });
 
 app.post('/auth', controller.user.authUser);
+
+app.get('/user', verify, controller.user.user);
+
+app.get('/logout', verify, controller.user.logout);
+
+app.get('/cover', verify, controller.cover.covers);
+
+app.get('/quotation', verify, controller.quotation.quotations);
+
+app.post('/quotation', verify, controller.quotation.newQuotation);
+
+app.get('/tender', verify, controller.tender.tenders);
+
+app.post('/tender', verify, controller.tender.newTender);
+
+app.get('/policy', verify, controller.policy.policys);
+
+app.post('/policy', verify, controller.policy.newPolicy);
