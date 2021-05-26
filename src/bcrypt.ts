fb3b979038e73cd1b7212ac6c5a1e410bf14435b
caiopovila <caiopovila@gmail.com>
2021-05-26 15:36:46 -0300

    ajustes de interface

--- src/bcrypt.ts
+++ src/bcrypt.ts
@@ -3,13 +3,13 @@ import * as bcrypt from 'bcrypt';
 
 class Bc {
 
-    encryptPassword(pass: string, saltRounds = 10): Promise<string> {
+    encryptPassword(pass: String, saltRounds = 10): Promise<String> {
         const salt = bcrypt.genSaltSync(saltRounds);
         const hash = bcrypt.hashSync(pass, salt);        
         return hash;
     };
 
-    async compare(srtPass: string, hash: string): Promise<Boolean> {
+    async compare(srtPass: String, hash: String): Promise<Boolean> {
         return await bcrypt.compare(srtPass, hash);
     };
 };
