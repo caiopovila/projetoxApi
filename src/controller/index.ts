a1ced8675373a13498eb8efb35700452434fc470
caiopovila <caiopovila@gmail.com>
2021-05-26 15:38:06 -0300

    indice de controle

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,4 +1,11 @@
 import * as userController from './userController';
-
+import * as tenderController from './tenderController';
+import * as quotationController from './quotationController';
+import * as policyController from './policyController';
+import * as coverController from './coverController';
 
 export { userController as user };
+export { coverController as cover };
+export { tenderController as tender };
+export { quotationController as quotation };
+export { policyController as policy };
